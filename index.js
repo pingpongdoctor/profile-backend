3c58b52b0908ab55f6126f81c17d15f0d9467fe7
Simon Tran <thanhnhantran1501@gmail.com>
2023-02-08 13:40:09 -0800

    fix bugs

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const app = express();
 const cors = require("cors");
 const connectDB = require("./config/db");
 const projectRoute = require("./routes/projectRoute");
-const { createProject } = require("./script");
 const PORT = process.env.PORT || 8080;
 //CONNECT DATABASE
 connectDB();
-createProject();
 //USE MIDDLEWARES
 app.use(cors());
 app.use(express.json());
