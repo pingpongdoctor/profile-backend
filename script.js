b4ecd0debf1dd0f65f5d54bd5f7d2ce1edaa2b38
Simon Tran <thanhnhantran1501@gmail.com>
2023-02-08 16:01:21 -0800

    fix bugs

--- script.js
+++ script.js
@@ -7,14 +7,18 @@ exports.createProject = async () => {
       name: "Simon Profile",
       description:
         "Simon Profile project is a MERN application that helps you know more about me, take a look at my projects and contact me easily",
-      deployment_link: "https://bandsite-platform.netlify.app/",
+      deployment_link: "https://simonprofile.netlify.app",
       tech_stack: {
         frontend: [
-          "Node.js, ExpressJS, ReactJS, AOS, React Type Animation,MUI Material, EmailJS, MongoDB + Mongoose, REST API",
+          "ReactJS",
+          "AOS",
+          "React Type Animation",
+          "MUI Material",
+          "EmailJS",
         ],
+        backend: ["Node.js", "ExpressJS", "MongoDB + Mongoose", "REST API"],
       },
     });
-
     console.log(newProject);
   } catch (e) {
     console.log(e);
